9447f49b2649aba2ba942ef5e99662cd5705ba6a
pickman1 <34723284+pickman1@users.noreply.github.com>
2022-09-04 13:25:51 +0200

    Update UI.gd

--- PiratScroller/Scripts/UI.gd
+++ PiratScroller/Scripts/UI.gd
@@ -3,6 +3,9 @@ extends Node
 onready var healthBar : TextureProgress = $Hbar
 onready var coinCounter : Label =  $Counter/Label
 
+func _ready():
+	update_coin_text()
+
 func update_health_bar (curHp, maxHp):
 	healthBar.max_value = maxHp
 	healthBar.value = curHp
@@ -11,6 +14,6 @@ func update_health_bar (curHp, maxHp):
 func update_coin_text ():
 	var player_vars = get_node("/root/PlayerVariables")
 	var coins = player_vars.coins
-	#coinCounter.text =  String(coinCounter)
+	coinCounter.text =  String(coins)
 
 
